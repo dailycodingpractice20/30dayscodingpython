c436767697f607677542ed2f033b6ff3a9d3534b
coding_daily <nguyendinhit1860@gmail.com>
2024-07-02 22:07:28 +0700

    day 01

---
+++ day_01.py
@@ -0,0 +1,28 @@
+# introduction
+# Day 1 - 30 Days of Python Daily Coding Challenge
+
+print("Introduction")
+print("Day 1 - 30 Days of Python Daily Coding Challenge")
+print("Start")
+
+print(2 + 3) #addition (+)
+print(3 - 1) #subtraction (-)
+print( 2 * 3) #multiplication (*)
+print(3 / 2) #division (/)
+print (3 ** 2) #exponentiation (**)
+print(3 % 2) #modulus (%)
+print(3 // 2) #floor division (//
+
+
+# Checking data type
+print(type(10)) #int
+print(type(3.14)) #float
+print(type(1 + 3j)) #complex
+print(type('hello Python')) #String
+print(type([2,3,4])) #list
+print(type({'type': "object"})) #Dictionary
+print(type({1.2, 3.14})) #Set
+print(type((1.2, 3.14))) #Tuple
+
+print("End")
+print("Thanks for watching")
