e3a3e4f7115708faa0d6a57dad14ffabacdd209e
Daily_Coding_Practice <nguyendinhit1806@gmail.com>
2024-07-03 20:14:55 +0700

    day 02

---
+++ day_02.py
@@ -0,0 +1,47 @@
+# Variable in Python
+
+print("Variable in Python")
+print("Day 2 - 30 Days of Python Daily Coding Challenge")
+print("Start")
+
+first_name = "John"
+last_name = "Nguyen"
+country = "VietNam"
+city = "HaNoi"
+age = 30
+is_married = True
+skill = ["HTML", "Python", "C/c++"," Java", "Css"]
+person_info ={
+    'first_name' : 'Nguyen',
+    'last_name' : 'Tony',
+    'country': 'VietNam',
+    'city': 'HCM'
+}
+
+# Print value stored in variable
+
+print("first_name: ",first_name)
+print("first_name_length: ", len(first_name))
+print("last_name: ", last_name)
+print("last_name_length: ", len(last_name))
+print('country: ', country)
+print('city: ', city)
+print('age: ', age)
+print('is_married: ', is_married)
+print('skill: ', skill)
+print('person_info: ', person_info)
+
+# Declaration of multiple variables
+
+first_name_m, last_name_m, country_m, city_m, age_m, is_married_m = "John", "Nguyen", "VietNam", "HaNoi", 30, True
+print(first_name_m, last_name_m, country_m, city_m, age_m, is_married_m)
+print(type(first_name_m), type(last_name_m), type(country_m), type(city_m), type(age_m), type(is_married_m))
+print('first_name_m: ', first_name_m)
+print('last_name_m: ', last_name_m)
+print('country_m: ', country_m)
+print("age_m: ", age_m)
+print("is_married_m: ", is_married_m)
+
+print("End")
+print("Thanks for watching")
+
