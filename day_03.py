2e1862586283397e215541ec9743810329164c7d
Daily_Coding_Practice <nguyendinhit1806@gmail.com>
2024-07-06 09:29:26 +0700

    day 03

---
+++ day_03.py
@@ -0,0 +1,88 @@
+# Operation in python
+# Day 3 - 30 Days of Python Daily Coding Challenge
+
+print("Introduction")
+print("Day 3 - 30 Days of Python Daily Coding Challenge")
+print("Start")
+
+print('Addition (+)', 1 + 2)
+print('Subtraction (-)', 5-4)
+print('Multiplication (*)', 2 * 5)
+print('Division (/)', 6/3)
+print('Division (/)', 5/3)
+print('Exponentiation (**)', 3**2)
+print('Modulus (%)', 3 % 2)
+print('Floor Division (//)', 7 //2)
+
+# floating number
+print("Floating number", 3.14)
+print("Floating number", 3.0)
+
+#complex number 
+print('complex number', 1 + 2j)
+print('Multiplication (*)', 1 + 2j * 3)
+
+# declare the variable
+a = 4
+b = 3
+
+# Operation
+total = a + b
+diff = a -b
+product = a* b
+division = a/b
+remainder = a % b
+exp = a**b
+floor_division = a // b
+
+print("total: ", total)
+print("diff: ", diff)
+print("product: ", product)
+print("division: ", division)
+print("remainder: ", remainder)
+print("exp: ", exp)
+print("floor_division: ", floor_division)
+
+# calculating area of a rectangle
+length = 30
+width = 10
+area = length * width
+print("area: ", area)
+
+# calculating area of a circle
+radius = 30
+pi = 3.14
+area = pi * radius**2
+print("area: ", area)
+
+# calculating a weight
+mass = 75
+gravity = 9.8
+weight = mass * gravity
+print("weight: ", weight)
+
+# check condition
+print(3>2)
+print(3>=2)
+print(3==2)
+print(3!=2)
+print(2<=3)
+
+print(len('hello') == len("ketty"))
+print(len('hello') != len("kiitty"))
+print(len('hello') < len("kiitty"))
+
+# boolean comparison
+print(3 > 2 and 1 < 2)
+print(3 > 2 or 1 < 2)
+print(not(3 > 2 or 1 < 2))
+print(3 > 2 and not(1 < 2 or 5 < 4))
+print(3 > 2 and not(1 < 2 or 5 < 4))
+
+print( not True)
+print(not False)
+print(not not True)
+print(not not False)
+
+print("End")
+print("Thanks for watching")
